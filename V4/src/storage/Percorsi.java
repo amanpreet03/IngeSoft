834a4737b4abfc224d43fb445699956b7c014b47
aman200373-max <aman200373@gmail.com>
2026-05-26 10:56:38 +0200

    modifica finale user json

--- V4/src/storage/Percorsi.java
+++ V4/src/storage/Percorsi.java
@@ -3,9 +3,9 @@ package storage;
 // percorsi dei file JSON usati dall'applicazione
 // modificare qui se si sposta la cartella json
 public class Percorsi {
-    public static final String AMBITO       = "json/ambito.json";
+    public static final String AMBITO       = "json/ambito_territoriale.json";
     public static final String UTENTI       = "json/users.json";
-    public static final String PIANO        = "json/piano.json";
+    public static final String PIANO        = "json/piano_visite.json";
     public static final String STORICO      = "json/storico.json";
     public static final String PRENOTAZIONI = "json/prenotazioni.json";
 
